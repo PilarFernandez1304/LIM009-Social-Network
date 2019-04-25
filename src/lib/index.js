ce4a4cf6b745787d27871c4699131eb0f1073922
lorena <lugotlorenay@gmail.com>
2019-04-25 08:06:37 -0500

    pruebas de autenticacion de usuarios con email-password y con facebook

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,12 @@
 // aqui exportaras las funciones que necesites
 
-export const myFunction = () => {
-  // aqui tu codigo
+export const config = {
+    apiKey: "AIzaSyCyGEFNsa3tdjepmFh5S6yhM0-FCePDjF0",
+    authDomain: "taxi-app-db.firebaseapp.com",
+    databaseURL: "https://taxi-app-db.firebaseio.com",
+    projectId: "taxi-app-db",
+    storageBucket: "taxi-app-db.appspot.com",
+    messagingSenderId: "387581643214"  
 }
+
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,83 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+import { config } from './lib/index.js';
 
-myFunction();
+firebase.initializeApp(config);
+
+const logInBtn = document.getElementById('log-in-btn');
+const signInBtn = document.getElementById('sign-in-btn');
+const logOutBtn = document.getElementById('log-out-btn');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const facebookBtn = document.getElementById('log-in-fb');
+
+logInBtn.addEventListener('click', e => {
+	const email = emailInput.value;
+	const password  = passwordInput.value;
+	const auth = firebase.auth();
+
+	const promise = auth.signInWithEmailAndPassword(email, password);
+	promise.catch(e => console.log(e.message));
+});
+
+signInBtn.addEventListener('click', e => {
+	const email = emailInput.value;
+	const password  = passwordInput.value;
+	// toDo: escribir funcion que valide el email y el password
+	const auth = firebase.auth();
+
+	const promise = auth.createUserWithEmailAndPassword(email, password);
+	promise.catch(e => console.log(e.message));
+});
+
+logOutBtn.addEventListener('click', e => {
+	firebase.auth().signOut();
+});
+
+firebase.auth().onAuthStateChanged( firebaseUser => {
+	if (firebaseUser) {
+		console.log(firebaseUser);
+	} else {
+		console.log('no logueado');
+	}
+});
+
+
+// inicio de sesion con facebook
+const authFacebook = () => {
+	const provider = new firebase.auth.FacebookAuthProvider();
+	firebase.auth().signInWithPopup(provider).then(result => {
+		console.log(result.user.displayName);
+	})
+	.catch(error => {
+		console.log(error);
+	});
+}
+
+facebookBtn.addEventListener('click', () => {
+	authFacebook();
+});
+
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '488881294982650',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v3.2'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+    FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+});
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
