8c8145f4d065d59ada03876cb3784b0be56af50c
PilarFernandez1304 <ccentepilar@gmail.com>
2019-05-03 18:11:04 -0500

    actualizado

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
+import config from './lib/index.js';
+import { initRouter } from "./router.js";
 
+window.addEventListener('load', () => {
+  firebase.initializeApp(config);
+  initRouter();
+});
+
+/*
+logOutBtn.addEventListener('click', e => {
+	firebase.auth().signOut();
+});
+firebase.auth().onAuthStateChanged( firebaseUser => {
+	if (firebaseUser) {
+		console.log(firebaseUser);
+	} else {
 		console.log('no logueado');
 	}
 });
